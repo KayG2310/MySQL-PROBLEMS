a0d11aac14fede04d943299fdf489f8b49166b04
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-08-14 01:58:32 +0530

    round to decimal places

---
+++ Leetcode/1211. Queries Quality and Percentage.sql
@@ -0,0 +1,3 @@
+-- Write your PostgreSQL query statement below
+select query_name, round(sum(rating::numeric/position)/count(query_name), 2) as quality, 
+round(((sum(case when rating < 3 then 1 else 0 end))*100.0/count(query_name)), 2) as poor_query_percentage from Queries group by query_name;  
