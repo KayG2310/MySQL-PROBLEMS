c342bda1c3a8bdc6f0727bb3476be812f230d438
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-08-14 02:19:52 +0530

    imp use of coalesce and left join

---
+++ Leetcode/1251. Average Selling Price.sql
@@ -0,0 +1,8 @@
+SELECT
+    p.product_id,
+    ROUND(COALESCE(SUM(p.price::numeric * us.units) / SUM(us.units),0),2) AS average_price
+FROM Prices AS p
+LEFT JOIN UnitsSold AS us 
+ON p.product_id = us.product_id 
+AND us.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
