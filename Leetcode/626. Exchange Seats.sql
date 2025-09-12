7fd04da139e8b74cd6d80a10f726d79561588b40
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2025-09-12 20:20:56 +0530

    creative thinking

---
+++ Leetcode/626. Exchange Seats.sql
@@ -0,0 +1,6 @@
+select (
+    case when id%2 = 1 and id = (select count(*) from Seat) then id 
+    when id%2 = 1 then id+1
+    when id%2 = 0 then id-1 
+end) as id, student from Seat
+order by id;
